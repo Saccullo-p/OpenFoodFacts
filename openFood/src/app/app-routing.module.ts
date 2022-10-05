83d27b00f471a7e128cf86ab437a953b8ab99b37
Saccullo-p <saccullo.pietro@iisgalvanimi.edu.it>
2022-10-05 08:33:08 +0000

    punto2

--- openFood/src/app/app-routing.module.ts
+++ openFood/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaratteristicheComponent } from './caratteristiche/caratteristiche.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'caratteristiche/:id', component: CaratteristicheComponent },
+  { path: '',   redirectTo: '/search', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- openFood/src/app/app.module.ts
+++ openFood/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CaratteristicheComponent } from './caratteristiche/caratteristiche.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CaratteristicheComponent
+    CaratteristicheComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
